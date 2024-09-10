0619e72338a3d1149f6db0ca63800cea00bb9395
Charles Morin <charles.mor@outlook.fr>
2024-09-10 12:29:08 -0400

    Added boilerplate code

--- src/main/java/com/pinax/kafka/paymentgateway/connect/sink/PaymentGatewaySinkConfig.java
+++ src/main/java/com/pinax/kafka/paymentgateway/connect/sink/PaymentGatewaySinkConfig.java
@@ -8,28 +8,26 @@ public class PaymentGatewaySinkConfig {
     public static final String BUFFER_SIZE = "bufferSize";
     public static final String BACTH_INTERVAL = "batchInterval";
 
-    public static ConfigDef config() {
-        return new ConfigDef()
-                .define(ENDPOINT,
-                        ConfigDef.Type.STRING,
-                        "http://localhost:8080",
-                        ConfigDef.Importance.HIGH,
-                        "Payment Gateway Endpoint")
-                .define(TOKEN,
-                        ConfigDef.Type.STRING,
-                        "token",
-                        new PaymentGatewaySinkConfigValidator(),
-                        ConfigDef.Importance.HIGH,
-                        "Payment Gateway Token")
-                .define(BUFFER_SIZE,
-                        ConfigDef.Type.INT,
-                        100,
-                        ConfigDef.Importance.HIGH,
-                        "Buffer Size")
-                .define(BACTH_INTERVAL,
-                        ConfigDef.Type.LONG,
-                        1000,
-                        ConfigDef.Importance.HIGH,
-                        "Batch Interval");
-    }
+    public static final ConfigDef CONFIG_DEF = new ConfigDef()
+            .define(ENDPOINT,
+                    ConfigDef.Type.STRING,
+                    "http://localhost:8080",
+                    ConfigDef.Importance.HIGH,
+                    "Payment Gateway Endpoint")
+            .define(TOKEN,
+                    ConfigDef.Type.STRING,
+                    "token",
+                    new PaymentGatewaySinkConfigValidator(),
+                    ConfigDef.Importance.HIGH,
+                    "Payment Gateway Token")
+            .define(BUFFER_SIZE,
+                    ConfigDef.Type.INT,
+                    100,
+                    ConfigDef.Importance.HIGH,
+                    "Buffer Size")
+            .define(BACTH_INTERVAL,
+                    ConfigDef.Type.LONG,
+                    1000,
+                    ConfigDef.Importance.HIGH,
+                    "Batch Interval");
 }
