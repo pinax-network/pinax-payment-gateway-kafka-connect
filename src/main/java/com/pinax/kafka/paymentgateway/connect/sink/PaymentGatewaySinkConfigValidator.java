a1bfe0ffa1adb1b237e6234b71ff51bcafccfc88
Charles Morin <charles.mor@outlook.fr>
2024-09-10 12:32:48 -0400

    Added config validation

--- src/main/java/com/pinax/kafka/paymentgateway/connect/sink/PaymentGatewaySinkConfigValidator.java
+++ src/main/java/com/pinax/kafka/paymentgateway/connect/sink/PaymentGatewaySinkConfigValidator.java
@@ -1,6 +1,6 @@
 package com.pinax.kafka.paymentgateway.connect.sink;
 
-import java.util.List;
+import java.net.*;
 
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigException;
@@ -9,6 +9,30 @@ public class PaymentGatewaySinkConfigValidator implements ConfigDef.Validator {
 
     @SuppressWarnings("unchecked")
     public void ensureValid(String name, Object value) {
+        if (name.equals(PaymentGatewaySinkConfig.ENDPOINT)) {
+            try {
+                URI uri = new URI((String) value);
+                String host = uri.getHost();
+                if (host == null || host.isEmpty()) {
+                    throw new ConfigException(name, value, "Not a valid URL");
+                }
 
+                int port = uri.getPort();
+                if (port == -1) {
+                    throw new ConfigException(name, value, "Not a valid URL");
+                }
+
+                String scheme = uri.getScheme();
+                if (scheme == null || !scheme.equals("http")) {
+                    throw new ConfigException(name, value, "Not a valid URL");
+                }
+            } catch (Exception e) {
+                throw new ConfigException(name, value, "Not a valid URL");
+            }
+        } else if (name.equals(PaymentGatewaySinkConfig.TOKEN)) {
+            if (value.toString() == null || value.toString().isEmpty()) {
+                throw new ConfigException(name, value, "Token cannot be empty");
+            }
+        }
     }
 }
